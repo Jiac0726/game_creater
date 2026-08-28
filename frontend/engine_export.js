09e3b3d213ef71efc6a054c6e784399bd50ec145
Jiac0726 <131017222+Jiac0726@users.noreply.github.com>
2026-08-28 14:17:43 +0800

    feat: load game ready 2d resource workspace

--- frontend/engine_export.js
+++ frontend/engine_export.js
@@ -45,5 +45,6 @@
   loadExtension("/asset_workflow.js");
   loadExtension("/asset_workflow_advanced.js");
   loadExtension("/asset_runtime.js");
+  loadExtension("/asset_2d.js");
   loadExtension("/store.js");
 })();
