6b073c5a404e9cf48cce244827fa937a1dfa5201
Jiac0726 <131017222+Jiac0726@users.noreply.github.com>
2026-08-28 15:35:58 +0800

    feat: load scene composer workspace

--- frontend/engine_export.js
+++ frontend/engine_export.js
@@ -46,5 +46,6 @@
   loadExtension("/asset_workflow_advanced.js");
   loadExtension("/asset_runtime.js");
   loadExtension("/asset_2d.js");
+  loadExtension("/scene_composer.js");
   loadExtension("/store.js");
 })();
